765679ab911cf7e0d07121734ac522ee78b4ae37
Atsushi <atsushi@moongift.jp>
2021-06-22 13:51:35 +0900

    Initial Commit

---
+++ camera/cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraApp.swift
+//  camera
+//
+//  Created by Atsushi on 2021/06/22.
+//
+
+import SwiftUI
+
+@main
+struct cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
