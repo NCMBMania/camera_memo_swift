3fc15cfca77f51aef286b85dbe05beff5236bb9b
Atsushi <atsushi@moongift.jp>
2021-10-19 12:53:45 +0900

    Support Xcode13

---
+++ CameraMemo/CameraMemoApp.swift
@@ -0,0 +1,21 @@
+//
+//  CameraMemoApp.swift
+//  CameraMemo
+//
+//  Created by Atsushi on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct CameraMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                ContentView()
+            }.onAppear {
+            
+            }
+        }
+    }
+}
